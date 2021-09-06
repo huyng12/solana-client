75e6565a045972aeb2052c5b79835c146fb80642
Huy <touch@nguyenrk.com>
2021-09-06 11:26:28 +0700

    Fix: Wrong balance calculation

--- src/cmd/balance.ts
+++ src/cmd/balance.ts
@@ -1,8 +1,10 @@
 import { loadWallet } from "./wallet";
 import { connection } from "../connection";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 export const getBalance = async (walletFilePath: string) => {
 	const wallet = await loadWallet(walletFilePath);
-	const balance = await connection.getBalance(wallet.keypair.publicKey);
+	const lamports = await connection.getBalance(wallet.keypair.publicKey);
+	const balance = lamports / LAMPORTS_PER_SOL;
 	console.log(`Your current balance: ${balance} SOL`);
 };
