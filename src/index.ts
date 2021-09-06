3f3874fe2fd7403cfb82e03de78c8731c31d8f6d
Huy <touch@nguyenrk.com>
2021-09-06 11:00:19 +0700

    Chore: Update CLI interface

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,29 @@ import { fetchAccount } from "./cmd/account";
 
 yargs(hideBin(process.argv))
 	.command(
-		"generate <path>",
-		"Generate Solana wallet at path <path>",
+		"generate",
+		"Generate Solana wallet",
 		(yargs) =>
-			yargs.positional("path", {
-				describe: "Output wallet file path",
+			yargs.option("path", {
+				global: true,
+				alias: "p",
 				type: "string",
+				default: "wallet.json",
+				describe: "Wallet file path",
 			}),
-		(argv) => generateWallet(argv.path!)
+		(argv) => generateWallet(argv.path)
 	)
 	.command(
-		"account <path>",
+		"account",
 		"Fetch account information",
 		(yargs) =>
-			yargs.positional("path", {
-				describe: "Wallet file path",
+			yargs.option("path", {
+				global: true,
+				alias: "p",
 				type: "string",
+				default: "wallet.json",
+				describe: "Wallet file path",
 			}),
-		(argv) => fetchAccount(argv.path!)
+		(argv) => fetchAccount(argv.path)
 	)
 	.help().argv;
