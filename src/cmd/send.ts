68087235ed483e6d83877a37de07e2b97adbbd76
Huy <touch@nguyenrk.com>
2021-09-07 13:55:45 +0700

    Implement build transaction and broadcast

--- src/cmd/send.ts
+++ src/cmd/send.ts
@@ -24,13 +24,29 @@ export const sendSOL = async (
 	);
 
 	// Sign transaction and broadcast
-	const signers = [from];
-	const signature = await web3.sendAndConfirmTransaction(
-		connection,
-		tx,
-		signers
-	);
+	// const signers = [from];
+	tx.recentBlockhash = await connection
+		.getRecentBlockhash()
+		.then((r) => r.blockhash);
+	tx.sign(from);
 
+	if (!tx.signature) {
+		throw new Error("Can not sign the transaction");
+	}
+
+	let signature = tx.signature.toString("base64");
+	console.log(`Signature: ${signature}`);
+
+	// Send transaction
+	const wireTx = tx.serialize();
+	signature = await connection.sendRawTransaction(wireTx);
+	console.log(`Send transaction successfully, broadcasting...`);
+
+	// Broadcast transaction
+	await connection.confirmTransaction(signature);
+	console.log(`Broadcast transaction successfully`);
+
+	console.log("");
 	console.log(`Sent ${amount} SOL to ${receiverPubKeyRaw} successfully`);
 	console.log(`https://explorer.solana.com/tx/${signature}?cluster=devnet`);
 };
